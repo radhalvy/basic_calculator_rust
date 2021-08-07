c925e8c12505f69326d047dbfeb5375ba41be1a4
Radhalvy Bernabel <radhalvybernabel@gmail.com>
2021-08-07 19:28:49 -0400

    Nothing really relevant

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,19 @@ fn main() {
     match functions::get_operation() {
         // Addition
         1 => {
-            println!("{}", functions::get_first_num() + functions::get_second_num());
+            println!("The answer is {}", functions::get_first_num() + functions::get_second_num());
         }
         // Subtraction
         2 => {
-            println!("{}", functions::get_first_num() - functions::get_second_num());
+            println!("The answer is {}", functions::get_first_num() - functions::get_second_num());
         }
         // Multiplication
         3 => {
-            println!("{}", functions::get_first_num() * functions::get_second_num());
+            println!("The answer is {}", functions::get_first_num() * functions::get_second_num());
         }
         // Division
         4 => {
-            println!("{}", functions::get_first_num() / functions::get_second_num());
+            println!("The answer is {}", functions::get_first_num() / functions::get_second_num());
         }
         _ => {
             println!("Invalid operation.")
